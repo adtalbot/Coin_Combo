e79f85daf13bc53f48a832da50c080c9293b1db9
Guest User <student@epicodus.com>
2015-04-23 14:12:22 -0700

    it is outputing all of our results

--- lib/coin_combo.rb
+++ lib/coin_combo.rb
@@ -2,16 +2,21 @@ class Fixnum
   define_method(:coin_combo) do
     remainder = self
     coins = [25, 10, 5, 1]
-    change = []
+    change_a = []
     num_coins = 0
 
     until remainder.==(0)
       coins.each() do |coin|
         num_coins = remainder./(coin)
         remainder = remainder.-(coin.*(num_coins))
-        change.push(num_coins)
+        change_a.push(num_coins)
       end
     end
-    change
+    quarters = change_a[0]
+    dimes = change_a[1]
+    nickels = change_a[2]
+    pennies = change_a[3]
+
+    "Your change is: #{quarters} quarters, #{dimes} dimes, #{nickels} nickels, #{pennies} pennies."
   end
 end
